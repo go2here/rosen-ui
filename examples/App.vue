d9f404d773855714ddb6872c1953c713858f89f6
378900700@qq.com <378900700@qq.com>
2022-04-30 17:35:36 +0800

    3.3

--- examples/App.vue
+++ examples/App.vue
@@ -2,7 +2,26 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <Demo />
-    <m-card />
+    <m-card imgSrc="java.png" summary="剑指Java面试-Offer直通车 百度资深面试官授课" />
+    <br />
+    <m-card imgSrc="c.png" summary="C/C++气象数据中心实战，手把手教你做工业级项目">
+      <template #footer>
+        <div class="footer">
+          <div class="level">中级 · 336人报名</div>
+          <div class="price">￥568.00</div>
+        </div>
+      </template>
+    </m-card>
+    <br />
+    <m-card imgSrc="spring.png" :width="370" :imgHeight="90"
+      >从SpringCloud项目搭建到微服务持续集成交付，快速斩获分布式微服务企业级综合高阶技能
+      <template #footer>
+        <div class="footer-spring">
+          <div class="level">4步骤 · 4门课</div>
+          <div class="level">6342收藏</div>
+        </div>
+      </template>
+    </m-card>
   </div>
 </template>
 
@@ -21,5 +40,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.footer {
+  font-size: 12px;
+  padding: 0 8px;
+  text-align: left;
+}
+.level {
+  color: #9199a1;
+  margin-bottom: 8px;
+}
+.price {
+  color: #f01414;
+}
+.footer-spring {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 8px;
+  font-size: 12px;
 }
 </style>
