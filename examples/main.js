be5a113f6af720126bcdd880fce12114c245ecf3
378900700@qq.com <378900700@qq.com>
2022-05-02 16:47:42 +0800

    5.2

--- examples/main.js
+++ examples/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import '../components/css/demo.scss'
-import '../components/css/card.scss'
-import Demo from '../components/lib/demo/index'
-import Card from '../components/lib/card/index'
+// import '../components/css/demo.scss'
+// import '../components/css/card.scss'
+// import Demo from '../components/lib/demo/index'
+// import Card from '../components/lib/card/index'
 
-// Vue.component('Demo', Demo)
-Vue.use(Demo) // Demo.install -> Vue.component()
-Vue.use(Card)
+// // Vue.component('Demo', Demo)
+// Vue.use(Demo) // Demo.install -> Vue.component()
+// Vue.use(Card)
+
+// 全部引用
+import 'rosen-ui/dist/css/index.css'
+import RUI from 'rosen-ui'
+Vue.use(RUI)
 
 Vue.config.productionTip = false
 
