ca08d29d462c88c1174093340f184ebcdac5e80b
378900700@qq.com <378900700@qq.com>
2022-05-02 20:30:52 +0800

    5.2

--- examples/main.js
+++ examples/main.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// // 全部引用
-// import 'rosen-ui/dist/css/index.css'
-// import RUI from 'rosen-ui'
-// Vue.use(RUI)
+// 全部引用
+import 'rosen-ui/dist/css/index.css'
+import RUI from 'rosen-ui'
+Vue.use(RUI)
 
-// 按需引用
-import 'rosen-ui/dist/css/demo.css'
-import Demo from 'rosen-ui'
-Vue.use(Demo)
+// // 按需引用
+// import 'rosen-ui/dist/css/demo.css'
+// import Demo from 'rosen-ui'
+// Vue.use(Demo)
 
-// 按需引用
-import 'rosen-ui/dist/css/card.css'
-import card from 'rosen-ui'
-Vue.use(card)
+// // 按需引用
+// import 'rosen-ui/dist/css/card.css'
+// import card from 'rosen-ui'
+// Vue.use(card)
 
 Vue.config.productionTip = false
 
