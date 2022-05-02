de745278d8cf4ebcaa791e3ec537bc8327de7de6
378900700@qq.com <378900700@qq.com>
2022-05-02 17:41:09 +0800

    5.1

--- examples/main.js
+++ examples/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// import '../components/css/demo.scss'
-// import '../components/css/card.scss'
-// import Demo from '../components/lib/demo/index'
-// import Card from '../components/lib/card/index'
+import '../components/css/demo.scss'
+import '../components/css/card.scss'
+import Demo from '../components/lib/demo/index'
+import Card from '../components/lib/card/index'
 
-// Vue.use(Demo) // Demo.install -> Vue.component()
-// Vue.use(Card) // Card.install -> Vue.component()
+Vue.use(Demo) // Demo.install -> Vue.component()
+Vue.use(Card) // Card.install -> Vue.component()
 
-import '../components/css/index.scss'
-import RUI from '../components/lib/index'
-Vue.use(RUI)
+// import '../components/css/index.scss'
+// import RUI from '../components/lib/index'
+// Vue.use(RUI)
 
 Vue.config.productionTip = false
 
