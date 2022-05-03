fa65e84985a2667e0a4ce3eac3b60b2fb697e35a
378900700@qq.com <378900700@qq.com>
2022-05-04 00:30:05 +0800

    fb

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/go2here/rosen-ui.git master:gh-pages
+git push -f git@github.com:go2here/rosen-ui.git master:gh-pages
 
 cd -
