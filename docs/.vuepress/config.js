0e59352305afc74617d427499905c6c99f0f3453
378900700@qq.com <378900700@qq.com>
2022-05-04 08:49:41 +0800

    master

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,14 @@ module.exports = {
   base: '/rosen-ui/',
   title: 'Card组件库',
   themeConfig: {
+    nav: [{
+      text: 'Home', link: '/'
+    }, {
+      text: 'Github', link: 'https://go2here.github.io/rosen-ui/'
+    }, {
+      text: 'Npm', link: 'https://www.npmjs.com/package/rosen-ui'
+    }
+    ],
     sidebar: [
       '/',
       '/component-docs/card',
